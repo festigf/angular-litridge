6ff9f808358e8f443f4f894c5a07d9d5f27282b7
festigf <gianfranco.festi@marconirovereto.it>
2020-01-26 10:05:56 +0100

    1 file modified

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -21,33 +21,10 @@ export class DataService {
       "https://lit-ridge-34418.herokuapp.com/api/department"
     );
   }
+  
+  postDepartment(dep){
+
+  }
 }
 
-/*
-import { Component,OnInit } from '@angular/core';
-import { FormBuilder,FormGroup} from '@angular/forms';
-@Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
-})
-export class AppComponent implements OnInit {
-  name = 'Angular';
 
-  myForm: FormGroup;
-
-  constructor(fb: FormBuilder) {
-  this.myForm = fb.group({
-    'sku': ['ABC123']
-   });
-}
-
-ngOnInit(){
-
-}
-onSubmit(value: string): void {
-   console.log('you submitted value: ', value);
- }
-}
-
-*/
