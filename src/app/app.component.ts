67759076348575f1f8be4b019c7ccc0ece527fea
festigf <gianfranco.festi@marconirovereto.it>
2020-01-26 09:49:22 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,3 +19,32 @@ export class AppComponent  {
     )
   }
 }
+
+/*
+import { Component,OnInit } from '@angular/core';
+import { FormBuilder,FormGroup} from '@angular/forms';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent implements OnInit {
+  name = 'Angular';
+
+  myForm: FormGroup;
+
+  constructor(fb: FormBuilder) {
+  this.myForm = fb.group({
+    'sku': ['ABC123']
+   });
+}
+
+ngOnInit(){
+
+}
+onSubmit(value: string): void {
+   console.log('you submitted value: ', value);
+ }
+}
+
+*/
